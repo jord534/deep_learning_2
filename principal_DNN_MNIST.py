2a9e1193f44c9b17e2ba46111393fd4cf9df4ec2
Jordan Bonil <jordan.bonil@ensta-paris.fr>
2022-04-16 12:56:19 +0200

    progres

--- principal_DNN_MNIST.py
+++ principal_DNN_MNIST.py
@@ -6,7 +6,7 @@ def calcul_softmax(rbm, input_data):
     """
     will give the probabilities of input_data accoridng to rbm
     using softmax
-    """
+    """ 
     output = rbm.W.dot(input_data) + rbm.a
     return softmax(output)
 
@@ -20,6 +20,16 @@ def entree_sortie_reseau(dnn, input_data):
     layer_outputs.append(calcul_softmax(dnn.classification_layer, layer_outputs[-1] ))
     return layer_outputs
 
-def retropropagation(dnn, n_iterations, ):
+
+def binary_cross_entropy(y_hat, y):
+    n = y.shape[-1]
+    return 1/n * np.sum(y*np.log(y_hat), axis = -1)
+
+def retropropagation(dnn, n_iterations, learning_rate, batch_size, data_size, data_labels):
+    
+    # let's code the algoirthm for one layer to the next
+    network_output = entree_sortie_reseau(dnn, input_data) # !!! same problem : which data to train on? retropropagation needs to know
+    classes_hat = network_output[-1]
+    cost = binary_cross_entropy(classes_hat, data_labels) # !!! : need to one hot encode labels
 
     return 0
